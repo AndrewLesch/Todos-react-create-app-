f405c330ccbfd12120c547579a175d7ed889373e
AndrewLesch <Andreykabreamz@gmail.com>
2021-11-13 01:43:22 +0300

    add first components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./Todo/TodoList";
 
 function App() {
-  return (
+  const todos = [
+    {id: 1, completed: false, title: 'Апнуть грандмастера'},
+    {id: 2, completed: false, title: 'Поднять бабла'},
+    {id: 3, completed: false, title: 'Купить курсач'},
+  ]
 
+  return (
+    <div className="wrapper">
+      <h1>React Tutorial</h1>
+
+      <TodoList todos={todos}></TodoList>
+    </div>
   )
-}
+};
 
 export default App;
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TodoItem ({todo, index}) {
+    return (
+        <li>
+            <span>
+                <input type="checkbox" />
+                <strong>{index + 1}</strong>
+                {todo.title}
+            </span>
+
+            <button>&times;</button>
+        </li>
+    )
+}
